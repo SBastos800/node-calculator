05866449b56629bbfbbb16e3211ed6b9690699c1
SBastos800 <soniavsbastos@gmail.com>
2020-08-29 18:38:41 +0100

    added page not found to the router

--- calculator.js
+++ calculator.js
@@ -7,14 +7,19 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/bmiCalculator.html");
+    res.sendFile('/bmiCalculator.html', {root: __dirname});
 });
+
 
 app.post('/', (req, res) => {
     let weight = parseFloat(req.body.weight);
     let height = parseFloat(req.body.height);
     let bmi = weight/(height*height);
     res.send('Your BMI is ' + bmi);
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile('./404.html', {root: __dirname});
 });
 
 app.listen(3000, () => {
